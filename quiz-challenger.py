4e49f3d935d1a08e1455a8c6922e850f98979991
William Li <liweiliang1997@outlook.com>
2021-06-06 23:41:58 +0800

    fix errors

--- quiz-challenger.py
+++ quiz-challenger.py
@@ -17,11 +17,10 @@ POST_URL = BASE_URL + "/question/challengeverify/"
 PARAM = "&is_new=2"
 NUM_QUESTIONS = 5
 
-# INPUT YOUR COOKIE
 HEADERS = {
     "User-Agent": "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) "
                   "Chrome/91.0.4472.77 Mobile Safari/537.36",
-    "Cookie": "",
+    # "Cookie": "PHPSESSID=phpsessidphpsessidphpsessi",
     "Host": "hezuo.btime.com",
     "Accept": "*/*",
     "Accept-Language": "zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2",
@@ -30,7 +29,6 @@ HEADERS = {
     "Connection": "keep-alive",
     "Referer": "https://h5.btime.com/"
 }
-
 
 class QuizChallenger():
     def __init__(self, phone_number, interval_sec, rounds):
@@ -48,6 +46,7 @@ class QuizChallenger():
 
     def request_questions(self):
         try:
+            self.session = requests.session()
             response_home = self.session.get(self.home_url, timeout=20, headers=HEADERS)
             response_info = self.session.get(self.info_url, timeout=20, headers=HEADERS)
             self.questions = json.loads(self.session.get(self.quiz_url, timeout=20, headers=HEADERS).text)
@@ -81,28 +80,40 @@ class QuizChallenger():
         }
         print(colored("Waiting for submission...", "yellow"))
         time.sleep(20 + random.randint(0, 15))
-        receipt = requests.post(url=POST_URL, headers=HEADERS, data=json.dumps(sheet))
-        receipt_json = json.loads(receipt.text)
-        print(colored("Status:{}, Message:{}".format(receipt_json["status"], receipt_json["msg"]), "green"))
-        table = PrettyTable()
-        table.field_names = ["Question ID", "Type", "Answer", "Is_correct"]
-        for question in receipt_json["data"]["data"]:
-            table.add_row([question["question_id"], question["type"], question["answer"], question["is_correct"]])
-        print(colored(table, "green"))
+        try:
+            receipt = self.session.post(url=POST_URL, headers=HEADERS, data=json.dumps(sheet))
+            receipt_json = json.loads(receipt.text)
+            print(colored("Status:{}, Message:{}".format(receipt_json["status"], receipt_json["msg"]), "green"))
+            table = PrettyTable()
+            table.field_names = ["Question ID", "Type", "Answer", "Is_correct"]
+            for question in receipt_json["data"]["data"]:
+                table.add_row([question["question_id"], question["type"], question["answer"], question["is_correct"]])
+            print(colored(table, "green"))
+            self.session.close()
+        except Exception:
+            print(colored("Error in Submission Process", "red"))
+            return
+
+    def clear_sheet(self):
+        self.session.close()
+        self.questions.clear()
+        self.question_answers.clear()
+        self.question_id.clear()
 
     def run(self):
         for i in range(self.rounds):
+            print(colored("Round {} starting...".format(i+1), "yellow"))
             self.request_questions()
             self.check_answers()
             self.submit_sheet()
+            self.clear_sheet()
             time.sleep(self.interval_sec)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Following the CCP\'s Lead - Quiz Challenger')
-    parser.add_argument('--phone_number', type=str, default="00000000000", help='Input your phone number.')
-    parser.add_argument('--interval_sec', type=int, default=0,
-                        help='Input the interval between two attempts.')
+    parser.add_argument('--phone_number', type=str, default="12345678910", help='Input your phone number.')
+    parser.add_argument('--interval_sec', type=int, default=0, help='Input the interval between two attempts.')
     parser.add_argument('--rounds', type=int, default=1, help='Input how many rounds in sum you would like to pass.')
     parser.add_argument("--daily-challenge", action='store_true', help='Use to do daily challenge.')
     args = parser.parse_args()
@@ -117,4 +128,4 @@ if __name__ == '__main__':
             challenger.run()
             break
         else:
-            args.phone_number = input("Your phone number is illegal, please re-input:")
+            args.phone_number = input("Your phone number is illegal, please re-input({}):".format(3-attempt))
